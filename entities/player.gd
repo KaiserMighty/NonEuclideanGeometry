c7b88ce1e672a80ecdf080cbb070bf8b173f8ea9
Kaiser Mighty <unikmey@gmail.com>
2024-11-25 13:45:41 -0800

    Portals Setup

--- entities/player.gd
+++ entities/player.gd
@@ -16,16 +16,12 @@ func _ready():
 
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("Left", "Right", "Forward", "Backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
